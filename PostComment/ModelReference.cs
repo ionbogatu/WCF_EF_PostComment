0a6559c3529cff77ed0ca8d67788c0317fc42fb6
Ion Bogatu <ionbogatu@gmail.com>
2020-05-13 16:09:32 +0300

    Initial commit

---
+++ PostComment/ModelReference.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using PostComment.Entities;
+
+namespace PostComment
+{
+    public class ModelReference : DbContext
+    {
+        public ModelReference()
+            : base("name=ModelReference")
+        {
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Post>()
+                .HasMany(m => m.Comments)
+                .WithRequired(m => m.Post);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
